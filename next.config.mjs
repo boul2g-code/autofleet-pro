aec131f246160312afc0a98ae8f742c0b416fc46
boul2g-code <boul2g@gmail.com>
2026-05-30 04:01:22 +0200

    update: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['jspdf'],
-  turbopack: {
-    root: __dirname,
-  },
+  optimizeFonts: false,
+  images: { unoptimized: true },
 }
-
 export default nextConfig
