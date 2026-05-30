16171206a3e0a872d89ee639be2e8bd9e18300c5
boul2g-code <boul2g@gmail.com>
2026-05-30 15:09:53 +0200

    fix: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-// v6.1 - fixed duplicate insert
+// v6.2 - fixed for Next.js 16
 const nextConfig = {
   reactStrictMode: false,
-  optimizeFonts: false,
   images: { unoptimized: true },
 }
 export default nextConfig
