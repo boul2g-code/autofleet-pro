485b056937b218e619832a82b3694095ca484373
boul2g-code <boul2g@gmail.com>
2026-05-30 15:52:44 +0200

    feat: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-// v6.3 - inline vehicle view, no redirect timing issue
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  optimizeFonts: false,
   images: { unoptimized: true },
 }
 export default nextConfig
